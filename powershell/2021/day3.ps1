7521fc6a322a6ab10ae2c03dc55fef77cc3ef61b
coryjr96 <cory.j.mitchell.jr@outlook.com>
2022-04-27 20:46:28 -0400

    Finished python day3 2021 and refractored solution to powershell

--- powershell/2021/day3.ps1
+++ powershell/2021/day3.ps1
@@ -1,1 +1,99 @@
 ﻿#DAY 3 2021
+
+$script:data = (gc 'C:\Users\coryj\OneDrive\Desktop\Programming\advent of code\powershell\2021\day3input.txt')
+
+Function Do-PartOne {
+    $gamma = ""
+    $epsilon = ""
+    For($iter = 0; $iter -le 11; $iter++) {
+        $counter = @{Ones = 0; Zeros = 0}
+        ForEach($line in $data) {
+            if($line[$iter] -eq "0") {
+                $counter.Zeros += 1
+            } else {
+                $counter.Ones += 1
+            }
+        }
+        if($counter.Ones -gt $counter.Zeros) {
+            $gamma += "1"
+        } else {
+            $gamma += "0"
+        }
+    }
+    $stringiter = $gamma.ToCharArray()
+    ForEach($letter in $stringiter) {
+        if($letter -eq "0") {
+            $epsilon += "1"
+        } else {
+            $epsilon += "0"
+        }
+    }
+    $gammadec = [Convert]::ToInt32($gamma,2)
+    $epsilondec = [Convert]::ToInt32($epsilon,2)
+    Write-Host "Part One Answer:" ($gammadec * $epsilondec)
+}
+
+Function Do-PartTwo {
+    Function Get-Oxygen {
+        $newdata = $data
+        For($iter = 0; $iter -le 11; $iter++) {
+            $counter = @{Ones = 0; Zeros = 0}
+            $oxygen = @()
+            $common = "0"
+            ForEach($line in $newdata) {
+                if($line[$iter] -eq "1") {
+                    $counter.Ones += 1
+                } else {
+                    $counter.Zeros += 1
+                }
+            }
+            if($counter.Ones -ge $counter.Zeros) {
+                $common = "1"
+            }
+            ForEach($line in $newdata) {
+                if($line[$iter] -eq $common) {
+                    $oxygen += $line
+                }
+            }
+            $newdata = $oxygen
+            if($oxygen.Count -eq 1) {
+                $script:finaloxygen = [Convert]::ToInt32($oxygen[0],2)
+                return
+            }
+        }
+    }
+    Function Get-Scrubber {
+        $newdata = $data
+        For($iter = 0; $iter -le 11; $iter++) {
+            $counter = @{Ones = 0; Zeros = 0}
+            $scrubber = @()
+            $uncommon = "1"
+            ForEach($line in $newdata) {
+                if($line[$iter] -eq "1") {
+                    $counter.Ones += 1
+                } else {
+                    $counter.Zeros += 1
+                }
+            }
+            if($counter.Zeros -le $counter.ones) {
+                $uncommon = "0"
+            }
+            ForEach($line in $newdata) {
+                if($line[$iter] -eq $uncommon) {
+                    $scrubber += $line
+                }
+            }
+            $newdata = $scrubber
+            if($scrubber.Count -eq 1) {
+                $script:finalscrubber = [Convert]::ToInt32($scrubber[0],2)
+                return
+            }
+        }
+    }
+    Get-Oxygen
+    Get-Scrubber
+    Write-Host "Part Two Answer:" ($finalscrubber * $finaloxygen)
+}
+
+Do-PartOne
+Do-PartTwo
--- python/2021/day3.py
+++ python/2021/day3.py
@@ -16,6 +16,55 @@ def part1():
             gamma += "1"
         else:
             gamma += "0"
-    print(gamma)
+    for i in range(len(gamma)):
+        if gamma[i] == "0":
+            epsilon += "1"
+        else:
+            epsilon += "0"
+    return(int(gamma, 2) * int(epsilon, 2))
 
-part1()
+def part2():
+    def getoxygen(input):
+        for num in range(len(input[0])):
+            collector = {0:0, 1:0}
+            oxygen = []
+            common = "0"
+            for line in input:
+                if line[num] == "1":
+                    collector[1] += 1
+                else:
+                    collector[0] += 1
+            if collector[1] >= collector[0]:
+                common = "1"
+            for line in input:
+                if line[num] == common:
+                    oxygen.append(line)
+            input = oxygen
+            if len(oxygen) == 1:
+                break
+        return(oxygen[0])
+
+    def getscrubber(input):
+        for num in range(len(input[0])):
+            collector = {0:0, 1:0}
+            scrubber = []
+            uncommon = "1"
+            for line in input:
+                if line[num] == "1":
+                    collector[1] += 1
+                else:
+                    collector[0] += 1
+            if collector[0] <= collector[1]:
+                uncommon = "0"
+            for line in input:
+                if line[num] == uncommon:
+                    scrubber.append(line)
+            input = scrubber
+            if len(scrubber) == 1:
+                break
+        return(scrubber[0])
+    result = int(getoxygen(input), 2) * int(getscrubber(input), 2)
+    return(result)
+        
+print("Part One Answer:", part1())
+print("Part Two Answer:", part2())
